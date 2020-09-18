0bf94044cc6bd1de6bea064b4a7ec45872da1a1c
Jscholey <jscholey@protonmail.com>
2020-09-18 23:57:39 +0100

    postgresql

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, request
+import os
+import psycopg2
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+conn = psycopg2.connect(DATABASE_URL, sslmode="require")
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def main():
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE EVENTS
+        (ID INT PRIMARY KEY NOT NULL,
+        EVENT TEXT NOT NULL);
+        """)
     return("<h1>Hi there</h1>")
 
 @app.route("/page", methods=["GET"])
