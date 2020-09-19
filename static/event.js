f9fe2e6fba6cd7d580ae93e5c66ebfac0b562dba
Morgan Baglin-Clarke <m.baglinclarke@gmail.com>
2020-09-19 17:10:12 +0100

    i gave up on spacebar

--- static/event.js
+++ static/event.js
@@ -1,48 +1,28 @@
-var time;
-var timeStr;
+var time = 0;
+var timeStr = "0.00";
 
 var loop;
-
-var running;
 
 var usingTimer = false;
 
 function useTimer() {
 	usingTimer = !usingTimer;
 	if (usingTimer) {
-		time = 0;
-		timeStr = "0.00";
-		running = false;
-		document.getElementById("help").innerHTML = "Spacebar to start/stop";
-		document.getElementById("time").innerHTML = "0.00";
-		document.getElementById("engage").value = "Press if not using timer";
+		document.getElementById("engage").value = "Stop";
+		loop = setInterval(
+			function() {
+				time = time + 1;
+				var csStr = time.toString();
+				while (csStr.length < 3) {
+					csStr = "0" + csStr;
+				}
+				timeStr = csStr.slice(0, -2) + "." + csStr.slice(-2);
+				document.getElementById("time").innerHTML = timeStr;
+			},
+			10
+		);
 	} else {
-		document.getElementById("help").innerHTML = "";
-		document.getElementById("time").innerHTML = "";
-		document.getElementById("engage").value = "Press to use timer";
+		document.getElementById("engage").value = "Start";
+		clearInterval(loop);
 	}
 }
-
-document.onkeypress = function (e) {
-	e = e || window.event;
-	if (usingTimer) {
-		if (e.keyCode == 32 && !running) {
-			running = true;
-			loop = setInterval(
-				function() {
-					time = time + 1;
-					var csStr = time.toString();
-					while (csStr.length < 3) {
-						csStr = "0" + csStr;
-					}
-					timeStr = csStr.slice(0, -2) + "." + csStr.slice(-2);
-					document.getElementById("time").innerHTML = timeStr;
-				},
-				10
-			);
-		} else if (running) {
-			clearInterval(loop);
-			useTimer();
-		}
-	}
-};
